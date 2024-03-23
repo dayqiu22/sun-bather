528180bf3f9c8b0de18674cbebccfa43cdd10ab4
Daywell Qiu <daywellqiu@gmail.com>
2024-03-23 16:23:01 -0700

    added preferences input form

--- src/app/ui/preferences/box.tsx
+++ src/app/ui/preferences/box.tsx
@@ -1,9 +1,10 @@
 import { Panel, PanelGroup } from 'rsuite';
+import Selection from "../../ui/preferences/selection";
 
 export default function Box() {
     return (
         <Panel header="Preferences" bordered>
-            
+            <Selection />
         </Panel>
     );
 }
---
+++ src/app/ui/preferences/selection.tsx
@@ -0,0 +1,40 @@
+import { Radio, RadioGroup } from 'rsuite';
+import { Input, InputGroup } from 'rsuite';
+
+export default function Selection() {
+    return (
+        <div>
+            <div>
+                Ideal Temperature
+                <InputGroup>
+                    <Input />
+                    <InputGroup.Addon>&deg;C</InputGroup.Addon>
+                    <InputGroup.Addon>to</InputGroup.Addon>
+                    <Input />
+                    <InputGroup.Addon>&deg;C</InputGroup.Addon>
+                </InputGroup>
+            </div>
+            Sun Intensity
+            <div>
+                <RadioGroup name="sun" inline defaultValue="no">
+                    <Radio value="no">Mild</Radio>
+                    <Radio value="yes">Strong</Radio>
+                </RadioGroup>
+            </div>
+            Windiness
+            <div>
+                <RadioGroup name="wind" inline defaultValue="no">
+                    <Radio value="no">None</Radio>
+                    <Radio value="yes">Breezy</Radio>
+                </RadioGroup>
+            </div>
+            Rain OK?
+            <div>
+                <RadioGroup name="rain" inline defaultValue="no">
+                    <Radio value="no">Absolutely Not</Radio>
+                    <Radio value="yes">Tolerable</Radio>
+                </RadioGroup>
+            </div>
+        </div>
+    );
+}
