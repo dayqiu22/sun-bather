38905bebb8417fd3129aa473854aa9c06765db2e
fredsunstrum <fsuns060@students.cs.ubc.ca>
2024-03-23 19:50:51 -0700

    front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,27 @@
 import styles from "./page.module.css";
 import Start from './ui/landing/start';
+import { Grid, Col, Row } from 'rsuite';
+
 import Title from './ui/landing/title';
 
 require('dotenv').config();
 
 
 export default function Landing() {
-
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        Landing Page
-        <Title/>
-        <Start/>
-      </div>
-    </main>
+    <Grid fluid>
+      <Row className={styles.main}>
+        <Col xs={24}>
+          <div className={styles.description}>
+            This be a Landing Page
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={24}>
+          <Start/>
+        </Col>
+      </Row>
+    </Grid>
   );
 }
